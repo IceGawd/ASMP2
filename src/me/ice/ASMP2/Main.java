6fa7ae58389fdb409b1f0ddfdbc541defa283a46
IceGawd <avighna.kukreja@gmail.com>
2021-12-26 20:53:43 -0800

    it fookin works

--- src/me/ice/ASMP2/Main.java
+++ src/me/ice/ASMP2/Main.java
@@ -1,7 +1,20 @@
 package me.ice.ASMP2;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		
+	}
 	
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (label.equalsIgnoreCase("media")) {
+			sender.sendMessage("Yo what up im big dumb");
+			return true;
+		}
+		return false;
+	}
 }
