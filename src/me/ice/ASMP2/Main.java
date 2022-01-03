df0585ad3e738162cd2841350c5c9f4357726263
JexPurx <65154039+JexPurx@users.noreply.github.com>
2022-01-02 21:28:54 -0500

    Update Main.java

--- src/me/ice/ASMP2/Main.java
+++ src/me/ice/ASMP2/Main.java
@@ -1,20 +1,21 @@
 package me.ice.ASMP2;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-	@Override
-	public void onEnable() {
+	@Override 
+	public void onEnable() {	
+		this.getCommand("Media").setExecutor(new Media());
+
 		
 	}
-	
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if (label.equalsIgnoreCase("media")) {
-			sender.sendMessage("Yo what up im big dumb");
-			return true;
-		}
-		return false;
+
+	public void onDisable() {
+		
 	}
-}
+		}
+		
+
+
+
+
