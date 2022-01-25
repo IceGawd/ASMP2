2740e6b622db3e1c2e5ea1165e6453304da7682a
Tre Logan <tr3.logan@gmail.com>
2022-01-24 23:10:48 -0500

    Documentation, discharge speed, refactoring

--- src/me/ice/ASMP2/ability/BlockThrowAbility.java
+++ src/me/ice/ASMP2/ability/BlockThrowAbility.java
@@ -4,10 +4,6 @@ import me.ice.ASMP2.Main;
 import org.bukkit.Sound;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
@@ -15,11 +11,14 @@ import java.util.*;
 
 public class BlockThrowAbility {
 
+    private static final int blockPickupDistance = 4;
     private static Set<UUID> playersPerformingAbility = new HashSet<>();
 
     public static void test(Player player, Vector velocity) {
         var world = player.getWorld();
         var blockInHand = player.getInventory().getItemInMainHand();
+
+        //TODO Remove this code once misidentification issue is fixed.
         if (!blockInHand.getType().isBlock() || blockInHand.getType().isAir()) {
             player.sendMessage("The item in hand is not a block. (" + blockInHand.getType() + ")");
             return;
@@ -34,48 +33,67 @@ public class BlockThrowAbility {
         if (playersPerformingAbility.contains(player.getUniqueId()))
             return;
 
-        // Get the block the player is looking at. (Maximum 3 blocks away.)
-        var result = player.rayTraceBlocks(3);
+        // Get the block the player is looking at.
+        var result = player.rayTraceBlocks(blockPickupDistance);
 
-        Block hitBlock;
+        Block rayTracedBlock;
 
         // Ensure that ray-tracing returned a block. If the result is valid
         // play block break sound indicating that a block has been picked up.
-        if (result == null || (hitBlock = result.getHitBlock()) == null) return;
-        player.playSound(hitBlock.getLocation(), Sound.BLOCK_STONE_BREAK, 1.0f, 1.0f);
+        if (result == null || (rayTracedBlock = result.getHitBlock()) == null) return;
+        player.playSound(rayTracedBlock.getLocation(), Sound.BLOCK_STONE_BREAK, 1.0f, 1.0f);
 
-        if (!player.isBlocking()) {
-            player.sendMessage("Player is not blocking.");
+        if (!player.isBlocking())
             return;
-        } else player.sendMessage("Player is blocking.");
 
         playersPerformingAbility.add(player.getUniqueId());
 
         new BukkitRunnable() {
-            private int blockingTimer;
+            private int blockingDuration;
             private boolean fullyCharged;
 
             @Override
             public void run() {
+                // Check if the player is blocking. If they are, then they
+                // are charging their shot.
                 if (player.isBlocking()) {
-                    if (blockingTimer < 8) {
-                        blockingTimer++;
+                    if (blockingDuration < 8) {
+                        blockingDuration++;
                         player.playSound(player, Sound.BLOCK_AMETHYST_BLOCK_CHIME, 0.5f, 0.5f);
                     } else {
-                        // The players shot is fully charged.
+                        // Check if the players shot is fully charged. If it is, then
+                        // play a clicking sound. The boolean prevents the sound from being repeated while
+                        // the players shot remains fully charged.
                         if (!fullyCharged) {
                             fullyCharged = true;
                             player.playSound(player, Sound.UI_BUTTON_CLICK, 1.0f, 1.0f);
                         }
                     }
                 } else {
-                    // Spawn and discharge the block from the players position to where they are looking.
-                    var block = player.getWorld().spawnFallingBlock(player.getLocation(), hitBlock.getBlockData());
-                    block.setVelocity(player.getLocation().getDirection().multiply(2));
+                    // The blocking duration is divided by 4 to cap the maximum speed a shot can get charged
+                    // to 2.0.
+                    shootBlock(player, rayTracedBlock, blockingDuration / 4.0f);
+
+                    // Remove the player from the list of players that are currently performing this ability.
+                    // If the player isn't removed then they will repeatedly shoot blocks.
                     playersPerformingAbility.remove(player.getUniqueId());
+
+                    // Cancel this runnable task.
                     cancel();
                 }
             }
         }.runTaskTimer(Main.getInstance(), 5, 5);
+    }
+
+    /**
+     * Shoots a block from the players location in the direction they are facing at the specified speed.
+     *
+     * @param shooter The player shooting the block.
+     * @param block   The block being shot.
+     * @param speed   The speed of the block.
+     */
+    private static void shootBlock(Player shooter, Block block, float speed) {
+        var blockProjectile = shooter.getWorld().spawnFallingBlock(shooter.getLocation(), block.getBlockData());
+        blockProjectile.setVelocity(shooter.getLocation().getDirection().multiply(speed));
     }
 }
